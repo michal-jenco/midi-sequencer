af25a02c885d09b2277c64ab8a1389a2487a8cba
Michal Jenčo <michal.jenco.brno@gmail.com>
2018-04-07 19:25:12 +0200

    memory scrolling works on both listboxes at the same time, even with mouse

--- source/memory.py
+++ source/memory.py
@@ -13,12 +13,31 @@ class Memory(tk.Frame):
 
         self.sequences = []
 
-        self.listbox_indices = tk.Listbox(self, width=2)
-        self.listbox_sequences = tk.Listbox(self, width=35)
+        self.scrollbar = tk.Scrollbar(self, command=self._scroll_both)
+
+        self.height = 16
+        self.listbox_indices = tk.Listbox(self, width=2, height=self.height,
+                                          selectmode=tk.EXTENDED, yscrollcommand=self.scrollbar.set)
+        self.listbox_sequences = tk.Listbox(self, width=35, height=self.height,
+                                            selectmode=tk.EXTENDED, yscrollcommand=self.scrollbar.set)
+
+        self.listbox_sequences.bind('<Delete>', self._delete_selection)
+        self.listbox_sequences.bind('<MouseWheel>', self._scroll_both_mouse)
+
+    def _scroll_both(self, *args):
+        self.listbox_indices.yview(*args)
+        self.listbox_sequences.yview(*args)
+
+    def _scroll_both_mouse(self, event):
+        arg = -event.delta // 120, "units"
+        self.listbox_indices.yview_scroll(*arg)
+        self.listbox_sequences.yview_scroll(*arg)
 
     def show(self):
         self.listbox_indices.grid(row=5, column=4, pady=2)
         self.listbox_sequences.grid(row=5, column=5, pady=2)
+
+        self.scrollbar.grid(row=5, column=6, sticky="ens")
 
     def add_seq(self, seq):
         self.listbox_sequences.insert(tk.END, seq)
@@ -32,3 +51,25 @@ class Memory(tk.Frame):
 
     def get_all(self):
         return "Not implemented yet."
+
+    def save(self):
+        pass
+
+    def load(self):
+        pass
+
+    def _regenerate_indices(self):
+        self.listbox_indices.delete(0, tk.END)
+
+        for i in range(0, self.listbox_sequences.size()):
+            self.listbox_indices.insert(tk.END, i)
+
+    def _delete_selection(self, _):
+        items = self.listbox_sequences.curselection()
+
+        for pos, i in enumerate(items):
+            idx = int(i) - pos
+            self.listbox_sequences.delete(idx, idx)
+
+        self._regenerate_indices()
+
